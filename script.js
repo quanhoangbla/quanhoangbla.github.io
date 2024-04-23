c7ae11fd18375e3138637e74f2dec363beb39f66
HoangQuanGITHUB <mcinotgoodatgame@gmail.com>
2024-04-23 21:21:17 +0700

    skibidi

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-if (localStorage.getItem('username')){
-    var user=localStorage.getItem('username');
-    var pass=localStorage.getItem('password');
-    var users={}
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", "users.json", true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4) {
-            users = JSON.parse(rawFile.responseText);
-            if (users[user]!=pass){
-                window.location.href='login.html';
-            }
+var user=localStorage.getItem('username');
+var pass=localStorage.getItem('password');
+var users={}
+var rawFile = new XMLHttpRequest();
+rawFile.open("GET", "users.json", true);
+rawFile.onreadystatechange = function() {
+    if (rawFile.readyState === 4) {
+        users = JSON.parse(rawFile.responseText);
+        if (users[user]!=pass){
+            window.location.href='login.html';
         }
     }
-    rawFile.send();
-}else{
-    window.location.href='login.html';
 }
+rawFile.send();
