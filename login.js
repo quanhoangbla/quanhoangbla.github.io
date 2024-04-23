8ab26e3fd98f0a66f6fb930af22a1b180390c79f
HoangQuanGITHUB <mcinotgoodatgame@gmail.com>
2024-04-23 21:05:05 +0700

    fdafdas

---
+++ login.js
@@ -0,0 +1,22 @@
+function login(){
+    var user=document.getElementById("username").value;
+    var pass=document.getElementById("pwd").value;
+    var users={}
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "users.json", true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4) {
+            users = JSON.parse(rawFile.responseText);
+            localStorage.clear()
+            localStorage.setItem('username',user);
+            localStorage.setItem('password',pass);            
+            if (users[user]==pass){
+                alert("Login Succesful!");
+                window.location.href='index.html';
+            }else{
+                alert("Login Failed, Please try again.");
+            }
+        }
+    }
+    rawFile.send();
+}
---
+++ script.js
@@ -0,0 +1,18 @@
+if (document.cookie){
+    var user=localStorage.getItem('username');
+    var pass=localStorage.getItem('password');
+    var users={}
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "users.json", true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4) {
+            users = JSON.parse(rawFile.responseText);
+            if (users[user]!=pass){
+                window.location.href='login.html';
+            }
+        }
+    }
+    rawFile.send();
+}else{
+    window.location.href='login.html';
+}
