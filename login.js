652a89d13f9d3e0d7c3c46003fbf765b20752f45
HoangQuanGITHUB <mcinotgoodatgame@gmail.com>
2024-04-24 22:28:45 +0700

    fdas

--- login.js
+++ login.js
@@ -1,15 +1,16 @@
+var a=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 function login(){
     var user=document.getElementById("username").value;
     var pass=document.getElementById("pwd").value;
     var users={}
     var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", "users.json", true);
+    rawFile.open("GET", '.'+a[4**2-10]+'it'+'/'+a[10+2**2*5/(2+3)]+'bject'+a[20-2]+'/users.json', true);
     rawFile.onreadystatechange = function() {
         if (rawFile.readyState === 4) {
             users = JSON.parse(rawFile.responseText);
-            localStorage.clear()
-            localStorage.setItem('username',user);
-            localStorage.setItem('password',pass);            
+            sessionStorage.clear()
+            sessionStorage.setItem('username',user);
+            sessionStorage.setItem('password',pass);            
             if (users[user]==pass){
                 alert("Login Succesful!");
                 window.location.href='https://quanvippro.site/';
--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-if (localStorage.getItem('username')==null){
-    localStorage.setItem('username','')
+var a=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+if (sessionStorage.getItem('username')==null){
+    sessionStorage.setItem('username','')
 }
-if (localStorage.getItem('password')==null){
-    localStorage.setItem('password','')
+if (sessionStorage.getItem('password')==null){
+    sessionStorage.setItem('password','')
 }
-var user=localStorage.getItem('username');
-var pass=localStorage.getItem('password');
+var user=sessionStorage.getItem('username');
+var pass=sessionStorage.getItem('password');
 var users={}
 var rawFile = new XMLHttpRequest();
-rawFile.open("GET", "users.json", true);
+rawFile.open("GET", '.'+a[4**2-10]+'it'+'/'+a[10+2**2*5/(2+3)]+'bject'+a[20-2]+'/users.json', true);
 rawFile.onreadystatechange = function() {
     if (rawFile.readyState === 4) {
         users = JSON.parse(rawFile.responseText);
